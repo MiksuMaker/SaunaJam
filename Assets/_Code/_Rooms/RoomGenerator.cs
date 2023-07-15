c0e28ab6ade40ecb80ef607727c682b70d572f96
MiksuMaker <91052332+MiksuMaker@users.noreply.github.com>
2023-07-15 11:36:32 +0300

    add: Room generation works for all types of rooms

--- Assets/_Code/_Rooms/RoomGenerator.cs
+++ Assets/_Code/_Rooms/RoomGenerator.cs
@@ -122,7 +122,6 @@ public class RoomGenerator : MonoBehaviour
         // Now that unfinished rooms have been added, go through them, adding dead ends
         int stubCount = roomsWithUnfinishedConnections.Count;
         for (int i = 0; i < stubCount; i++)
-            //foreach (Room r in roomsWithUnfinishedConnections)
         {
             CheckDirectionsAndGenerateIfNecessary(roomsWithUnfinishedConnections[0], true);
         }
@@ -184,14 +183,18 @@ public class RoomGenerator : MonoBehaviour
 
     private TypeRoom DecideRoomType(bool isStart = false)
     {
-        int randNum = Random.Range(0, 3);
-        if (isStart) { randNum = Random.Range(1, 3); }
+        int maxNum = 5;
+        int randNum = Random.Range(0, maxNum);
+        if (isStart) { randNum = Random.Range(1, maxNum); }
 
 
         TypeRoom t = TypeRoom._1_deadEnd;
         if (randNum == 0) { t = TypeRoom._1_deadEnd; }
         else if (randNum == 1) { t = TypeRoom._2_corner; }
         else if (randNum == 2) { t = TypeRoom._2_straight; }
+
+        else if (randNum == 3) { t = TypeRoom._3_threeway; }
+        else if (randNum == 4) { t = TypeRoom._4_fourway; }
 
         return t;
     }
@@ -210,27 +213,58 @@ public class RoomGenerator : MonoBehaviour
             // STRAIGHTS : No need for randomization
             case (TypeRoom._2_straight, FromConnection.north): return Orientation.north;
             case (TypeRoom._2_straight, FromConnection.west): return Orientation.west;
-            case (TypeRoom._2_straight, FromConnection.east): return Orientation.east;
-            case (TypeRoom._2_straight, FromConnection.south): return Orientation.south;
+            case (TypeRoom._2_straight, FromConnection.east): return Orientation.west;
+            case (TypeRoom._2_straight, FromConnection.south): return Orientation.north;
 
             // CORNERS : Decide a direction
             case (TypeRoom._2_corner, FromConnection.north):
                 return GetRandomValidOrientation(new Orientation[2]
                                                    { Orientation.north,
-                                                                                                  Orientation.west});
+                                                     Orientation.west});
             case (TypeRoom._2_corner, FromConnection.west):
                 return GetRandomValidOrientation(new Orientation[2]
                                                     { Orientation.south,
-                                                                                                  Orientation.west});
+                                                      Orientation.west});
             case (TypeRoom._2_corner, FromConnection.east):
                 return GetRandomValidOrientation(new Orientation[2]
                                                     { Orientation.north,
-                                                                                                  Orientation.east});
+                                                      Orientation.east});
             case (TypeRoom._2_corner, FromConnection.south):
                 return GetRandomValidOrientation(new Orientation[2]
                                                    { Orientation.south,
-                                                                                                  Orientation.east});
+                                                     Orientation.east});
 
+            // THREEWAY : Decide a direction
+            case (TypeRoom._3_threeway, FromConnection.north):
+                return GetRandomValidOrientation(new Orientation[3]
+                                                   { Orientation.north,
+                                                     Orientation.west,
+                                                     Orientation.south});
+            case (TypeRoom._3_threeway, FromConnection.west):
+                return GetRandomValidOrientation(new Orientation[3]
+                                                   { Orientation.east,
+                                                     Orientation.west,
+                                                     Orientation.south});
+            case (TypeRoom._3_threeway, FromConnection.east):
+                return GetRandomValidOrientation(new Orientation[3]
+                                                   { Orientation.north,
+                                                     Orientation.west,
+                                                     Orientation.east});
+            case (TypeRoom._3_threeway, FromConnection.south):
+                return GetRandomValidOrientation(new Orientation[3]
+                                                   { Orientation.north,
+                                                     Orientation.east,
+                                                     Orientation.south});
+
+            // FOURWAY : Doesn't matter, but randomize still
+            case (TypeRoom._4_fourway, _):
+                return GetRandomValidOrientation(new Orientation[4]
+                                                    { Orientation.north,
+                                                      Orientation.west,
+                                                      Orientation.east,
+                                                      Orientation.south});
+
+            // DEFAULT
             default: return Orientation.south;
         }
     }
@@ -287,6 +321,28 @@ public class RoomGenerator : MonoBehaviour
             case (TypeRoom._2_straight, Orientation.west):
                 n = true; s = true;
                 break;
+
+            // THREEWAYS
+            case (TypeRoom._3_threeway, Orientation.north):
+                w = true;
+                break;
+            case (TypeRoom._3_threeway, Orientation.west):
+                s = true;
+                break;
+            case (TypeRoom._3_threeway, Orientation.east):
+                n = true;
+                break;
+            case (TypeRoom._3_threeway, Orientation.south):
+                e = true;
+                break;
+
+            // FOURWAYS
+            case (TypeRoom._4_fourway, _):
+                // No walls!
+                break;
+
+            // DEFAULT
+            default: Debug.LogWarning("Missed a roomtype!"); break;
         }
         #endregion
 
