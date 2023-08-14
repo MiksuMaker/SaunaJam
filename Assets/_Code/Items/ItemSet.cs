5dad614446510f67371871be2b46ae4edb638f5e
MiksuMaker <91052332+MiksuMaker@users.noreply.github.com>
2023-08-14 23:56:26 +0300

    add: ItemSet

---
+++ Assets/_Code/Items/ItemSet.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemSet : MonoBehaviour
+{
+    public int water = 0;
+    public int wood = 0;
+    public int stone = 0;
+    public int write = 0;
+    public int sauna = 0;
+}
